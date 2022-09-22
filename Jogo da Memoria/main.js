f0ee91ea6fd5a4cca77b862e5e286887b92fd110
Thalyta Greco <tgreco_isaias@hotmail.com>
2022-09-21 22:27:24 -0300

    links

---
+++ Jogo da Memoria/main.js
@@ -0,0 +1,100 @@
+
+
+const cardBoard = document.querySelector("#cardboard");
+var imgs = [
+    "crazy.svg",
+    "dor.svg",
+    "piscando.svg",
+    "risonho.svg",
+    "soneca.svg",
+    "triste.svg"
+];
+
+imgs = shuffle(imgs);
+
+let cardHTML = "";
+
+imgs.forEach(img => {
+  cardHTML += `<div class="memory-card" data-card="${img}">
+    <img class="front-face" src="img/${img}"/>
+    <img class="back-face" src="img/Zumbi.svg">
+  </div>`;
+});
+
+
+
+cardBoard.innerHTML = cardHTML + cardHTML;
+
+/** Fim da Renderização HTML */
+
+const cards = document.querySelectorAll(".memory-card");
+let firstCard, secondCard;
+let lockCards = false;
+
+
+function flipCard() {
+  if (lockCards) return false;
+  this.classList.add("flip");
+
+  if (!firstCard) {
+    firstCard = this;
+    return false;
+  }
+
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+  !isMatch ? unFlipCards() : resetCards(isMatch);
+}
+
+function unFlipCards() {
+  lockCards = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetCards();
+  }, 1000);
+}
+
+function resetCards(isMatch = false) {
+  if (isMatch) {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+  }
+
+  [firstCard, secondCard, lockCards] = [null, null, false];
+}
+
+
+
+//reiniciar jogo
+
+function restart() {
+    window.location.reload();
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard));
